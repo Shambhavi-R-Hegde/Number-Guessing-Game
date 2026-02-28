d0d3efd55b7908747361c6bb89b402cede8571fd
Shambhavi Hegde <Shambhavi.Hegde1@ibm.com>
2026-02-28 23:00:48 +0530

    Number Guessing Game v2 - Added difficulty levels and dynamic attempts

--- src/numberguessinggame/NumberGuessingGame.java
+++ src/numberguessinggame/NumberGuessingGame.java
@@ -6,17 +6,44 @@ import java.util.Scanner;
 public class NumberGuessingGame {
 
 	public static void main(String[] args) {
+		int maxRange,maxAttempt;
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Please select the difficulty level:");
+		System.out.println("1. Easy");
+		System.out.println("2. Medium");
+		System.out.println("3. Hard");
+		int choice=sc.nextInt();
+		switch(choice) {
+			case 1: 
+				maxRange=50;
+				maxAttempt=10;
+				break;
+			case 2:
+				maxRange=100;
+				maxAttempt=7;
+				break;
+			case 3:
+				maxRange=500;
+				maxAttempt=5;
+				break;
+			default :
+				System.out.println("Invalid choice! setting to medium");
+				maxRange=100;
+				maxAttempt=7;
+			}
+			
 		Random random=new Random();
-		int number=random.nextInt(100)+1;
+		int number=random.nextInt(maxRange)+1;
 		int attempts=0;
 		boolean guessedCorrectly=false;
+		System.out.println("Now game runs between 1 - "+maxRange + " with only "+maxAttempt+" guesses.");
 		
-		while(attempts<7&&!guessedCorrectly) {
-			System.out.println("Please Enter a guess a number between 1 to 100:");
+		while(attempts<maxAttempt&&!guessedCorrectly) {
+			System.out.println("Please guess the number:");
 			int guess=sc.nextInt();
 			attempts++;
-			if(guess>=1&&guess<=100) {
+			System.out.println("Attempts left: " + (maxAttempt - attempts));
+			if(guess>=1&&guess<=maxRange) {
 				if(guess>number) {
 					System.out.println("Too High😕");
 				} else if(guess<number) {
@@ -27,7 +54,7 @@ public class NumberGuessingGame {
 					guessedCorrectly=true;
 				}
 			}else {
-				System.out.println("you must enter a number between 0 to 100.");
+				System.out.println("you must enter a number between 1 - "+maxRange);
 			}
 		}
 			
